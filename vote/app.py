c5dc3e3b1f6da1f7b5fb7cfea2f1dbe5bdaa2873
Gourav Shah <gs@initcron.org>
2021-05-12 12:40:21 +0530

    v11

--- vote/app.py
+++ vote/app.py
@@ -8,7 +8,7 @@ import json
 option_a = os.getenv('OPTION_A', "AAA")
 option_b = os.getenv('OPTION_B', "BBB")
 hostname = socket.gethostname()
-version = 'v18'
+version = 'v11'
 
 app = Flask(__name__)
 
